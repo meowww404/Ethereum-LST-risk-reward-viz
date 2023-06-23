9019de360acae56aaa2ee7518bd64011dd23cd8b
meowww404 <l@miti.ai>
2023-06-23 14:13:38 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,162 @@
-import Image from 'next/image'
+'use client'
+
+import React, { useRef, useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import * as THREE from 'three';
+
+const ForceGraph3D = dynamic(() => import('react-force-graph').then(module => module.ForceGraph3D), { ssr: false });
 
 export default function Home() {
+  const [hoveredNode, setHoveredNode] = useState(null);
+  const [animationPaused, setAnimationPaused] = useState(false);
+
+  const handleNodeClick = (node) => {
+    if (node) {
+      setHoveredNode(node);
+      setAnimationPaused(true); // Pause animation when hovering over a node
+    }
+  };
+
+  const handleResumeAnimation = () => {
+    setAnimationPaused(false); // Resume animation
+    setHoveredNode(null); // Hide tooltip
+  };
+
+  const graphData = {
+    nodes: [
+      { 
+        id: 'LSD Strategy #1', 
+        avgAPR: '+12.21% annually',
+        avgGasCostEntry: '0.13ETH',
+        avgGasCostExit: '0.11ETH'
+      },
+      { 
+        id: 'LSD Strategy #2', 
+        avgAPR: '+10.35% annually',
+        avgGasCostEntry: '0.15ETH',
+        avgGasCostExit: '0.13ETH'
+      },
+      { 
+        id: 'LSD Strategy #3', 
+        avgAPR: '+14.70% annually',
+        avgGasCostEntry: '0.12ETH',
+        avgGasCostExit: '0.10ETH'
+      }
+    ],
+    links: [
+      { source: 'LSD Strategy #1', target: 'LSD Strategy #2' },
+      { source: 'LSD Strategy #2', target: 'LSD Strategy #3' }
+    ]
+  };
+  
+  // useEffect(() => {
+  //   setTooltipNode(hoveredNode); // Update tooltipNode when hoveredNode changes
+  // }, [hoveredNode]);
+
+  // // useEffect(() => {
+  // //   const handleMouseMove = (event) => {
+  // //     if (hoveredNode) {
+  // //       setTooltipPosition({ x: event.clientX, y: event.clientY });
+  // //     }
+  // //   };
+  
+  // //   document.addEventListener('mousemove', handleMouseMove);
+  
+  // //   return () => {
+  // //     document.removeEventListener('mousemove', handleMouseMove);
+  // //   };
+  // // }, [hoveredNode]);
+
+ 
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+       {hoveredNode && (
+        <div
+          style={{
+            position: 'absolute',
+            zIndex: 1,
+            top: '50%',
+            left: '35%',
+            transform: 'translate(95%, -50%)',
+            background: 'darkgrey',
+            color: 'black',
+            padding: '5vw',
+            borderRadius: 5,
+            fontSize: '15px',
+            pointerEvents: 'none', // Disable pointer events on tooltip to prevent interference
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    <strong>{hoveredNode.id}</strong>
+    <p>Avg. APR: {hoveredNode.avgAPR}</p>
+    <p>Avg. Gas Cost for Entry Position: {hoveredNode.avgGasCostEntry}</p>
+    <p>Avg. Gas Cost for Exit Position: {hoveredNode.avgGasCostExit}</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  </div>
+)}
+      <ForceGraph3D 
+        graphData={graphData}
+        nodeThreeObject={(node) => {
+          const geometry = new THREE.SphereGeometry(4, 16, 12); 
+          const material = new THREE.MeshStandardMaterial({ 
+            color: node === hoveredNode ? 'red' : 'salmon',
+            roughness: 0.5,
+            lightMap: null,
+            lightMapIntensity: 1,
+            emissiveIntensity: 1,
+            emissiveMap: null,
+          });
+          return new THREE.Mesh(geometry, material);
+        }}
+        linkDirectionalArrowLength={5.5}
+        linkDirectionalArrowRelPos={1}
+        linkDirectionalArrowColor={'black'}
+        linkOpacity={1}
+        linkCurvature={0.1}
+        backgroundColor={'lightgrey'}
+        onNodeClick={handleNodeClick}
+        enablePointerInteraction={!animationPaused} // Disable pointer interaction when animation is paused
+      />
+    
+       {/* <ForceGraph3D 
+          graphData={graphData}
+          nodeThreeObject={(node) => {
+            const geometry = new THREE.SphereGeometry(4, 16, 12); 
+            const material = new THREE.MeshStandardMaterial({ 
+              color: node === hoveredNode ? 'red' : 'salmon', // change color on hover
+              roughness: 0.5,
+              lightMap: null,
+              lightMapIntensity: 1,
+              emissiveIntensity: 1,
+              emissiveMap: null,
+            });
+            return new THREE.Mesh(geometry, material);
+          }}
+            
+          linkDirectionalArrowLength={5.5}
+          linkDirectionalArrowRelPos={1}
+          linkDirectionalArrowColor={'black'}
+          linkOpacity={1}
+          linkCurvature={0.1}
+          nodeRelSize={4}
+          backgroundColor={'lightgrey'}
+          onNodeHover={handleNodeClick}
+          enablePointerInteraction={!animationPaused} // Disable pointer interaction when animation is paused
+          onTick={() => setAnimationPaused(false)} // Resume animation on each tick
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+       /> */}
+        <button
+          onClick={handleResumeAnimation}
+          style={{
+            position: 'absolute',
+            top: 10,
+            right: 10,
+            zIndex: 1,
+        }}
+      >
+        {animationPaused ? 'Reset' : 'Animation Running'}
+      </button>
+      <div class="position: absolute"> Click on one of the nodes to see information. Click reset before clicking another node. Zoom in/out or drag to rotate the axis.</div>
     </main>
-  )
+  );
 }
